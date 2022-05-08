79eef618ec279d0bcb5ae1c77e9527baee219f20
Nicolas Camenisch <garnele007@gmail.com>
2022-05-08 14:10:16 -0400

    Use p10k iwith cons when using kitty

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -1,6 +1,6 @@
 # If not using SSH or terminal is iTerm -> use icons
 # else use a different theme with higher compatibility
-if [[ -z $SSH_CONNECTION || $LC_TERMINAL == "iTerm"* ]]; then
+if [[ -z $SSH_CONNECTION || $LC_TERMINAL == "iTerm"* || $TERM == "xterm-kitty"* ]]; then
 	source $ZDOTDIR/config/p10k/p10k_icons.zsh
 else
 	source $ZDOTDIR/config/p10k/p10k_simple.zsh
