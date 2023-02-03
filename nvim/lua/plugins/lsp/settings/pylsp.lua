0b66a3e09a05fdc6eee958b3863a7310eda5a04a
Nicolas Camenisch <garnele007@gmail.com>
2023-02-03 10:05:24 -0500

    nvim: update pylsp config

--- nvim/lua/plugins/lsp/settings/pylsp.lua
+++ nvim/lua/plugins/lsp/settings/pylsp.lua
@@ -4,6 +4,13 @@ return {
       plugins = {
         pycodestyle = {
           maxLineLength = 88,
+          ignore = {
+            "F841",
+            "E501",
+          },
+        },
+        pyflakes = {
+          enabled = false,
         },
       },
     },
